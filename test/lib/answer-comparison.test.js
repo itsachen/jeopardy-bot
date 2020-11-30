f9245e655f46a7eb11aaaa12f20d8c1c8d43b78d
Anthony Chen <pandaman0529@gmail.com>
2020-11-30 12:13:09 -0500

    Fix answer race condition (#9)
    * Fix answer race condition
    
    * remove punctuation too
    
    * fix some stuff, adds tests

---
+++ test/lib/answer-comparison.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AnswerComparison = require('../../lib/answer-comparison');
+
+describe('AnswerComparison', () => {
+
+  describe('compare', () => {
+
+    describe('with punctuation', () => {
+
+      it('ignores double quotes', () => {
+        const guess = 'W';
+        const actual = '"W"';
+        expect(AnswerComparison.compare(guess, actual)).to.eq(true);
+      });
+
+      it('ignores single quotes', () => {
+        const guess = 'as';
+        const actual = 'a\'s';
+        expect(AnswerComparison.compare(guess, actual)).to.eq(true);
+      });
+
+      it('ignores combination', () => {
+        const guess = 'twas the night before christmas';
+        const actual = '"\'Twas the Night Before Christmas" ("A Visit from St. Nicholas")"';
+        expect(AnswerComparison.compare(guess, actual)).to.eq(true);
+      });
+
+      it('ignores periods', () => {
+        const guess = 'c.s. lewis';
+        const actual = 'C.S. Lewis';
+        expect(AnswerComparison.compare(guess, actual)).to.eq(true);
+      });
+
+    });
+
+  });
+
+});
